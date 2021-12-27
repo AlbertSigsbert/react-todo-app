8198f6f4104a411d423dccc36e214c4a14cea454
Albert <ishengomaalbert@gmail.com>
2021-12-27 12:14:12 +0300

    Fixed a css bug

--- src/components/styled/NoTodos.styled.js
+++ src/components/styled/NoTodos.styled.js
@@ -4,4 +4,10 @@ export const StyledNoTodos = styled.div`
   margin: 30px auto;
   text-align: center;
   font-size: 1.2rem;
+
+  @media (max-width: 576px) {
+     img{
+       width: 70%;
+     }
+  }
 `;
